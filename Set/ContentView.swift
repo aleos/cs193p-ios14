70836419371927d1d745c49c5ed6aa2949576467
Alexander Ostrovsky <thealeos@gmail.com>
2022-07-21 00:40:05 +0300

    Add Set game project

---
+++ Set/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Alexander Ostrovsky on 21.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Set!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Alexander Ostrovsky on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
